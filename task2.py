78a046dbc4ede58a4f15325db5d1da0f90aa5516
ParkJames28 <park.jamespatrick28@gmail.com>
2024-10-03 11:23:48 +0800

    task2

---
+++ task2.py
@@ -0,0 +1,11 @@
+list_of_fruits = ["banana","apple","orange","manggo"]
+
+for data in list_of_fruits:
+
+    if data == "banana":
+        print ("hellow banana")
+    elif data== "apple":
+        print ("hellow apple")
+    else:
+        ("no")
+    
